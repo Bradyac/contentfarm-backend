cc884cecf911393972f78c52c44ec0754fff75d9
Brady <brady.cunningham@ryerson.ca>
2022-04-07 01:09:15 -0400

    Initial commit with generated project from Spring Initializr

---
+++ src/main/java/ca/bradyac/contentfarm/ContentFarmApplication.java
@@ -0,0 +1,13 @@
+package ca.bradyac.contentfarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentFarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentFarmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/bradyac/contentfarm/ContentFarmApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.bradyac.contentfarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentFarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
